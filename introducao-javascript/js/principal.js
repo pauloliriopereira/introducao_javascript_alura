c3890958b1c65b5ca395b37392915fb51a3a4082
paulolirio <paulolirio@n46v.schedulesoftware>
2018-08-10 15:48:26 -0300

    Merge branch 'master' of https://github.com/pauloliriopereira/introducao_javascript_alura

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,6 +1,5 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
-
 var trPaciente = document.querySelector('#primeiro-paciente');
 var tdPeso = trPaciente.querySelector('.info-peso');
 var tdAltura = trPaciente.querySelector('.info-altura');
@@ -9,8 +8,8 @@ var tdImc = trPaciente.querySelector('.info-imc');
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
 
-var pesoEhValido = true; // assumindo de boa fé que o peso é válido
-var alturaEhValida = true; // assumindo de boa fé que a altura é válida
+var pesoEhValido = true;
+var alturaEhValida = true;
 
 if(peso <=0 || peso >= 1000) {
    pesoEhValido = false;
