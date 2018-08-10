96f30e864f365aa65471b738f7c4cee8f9dc74d4
paulolirio <paulolirio@n46v.schedulesoftware>
2018-08-10 16:48:04 -0300

    Melhoria de código.

---
+++ introducao-javascript/js/form.js
@@ -0,0 +1,46 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var form = document.querySelector("#form-adiciona");
+  var paciente = obtemPacienteDoFormulario(form);
+  console.log(paciente);
+
+  var pacienteTr = document.createElement("tr");
+
+  pacienteTr.classList.add("paciente");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+  imcTd.textContent = calculaImc(peso, altura);
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
+});
+
+function obtemPacienteDoFormulario(form) {
+  var paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calculaImc(form.peso.value, form.altura.value)
+  }
+  return paciente;
+}
