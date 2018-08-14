6c93ad7b8847dd18716f6bc8459c6c7306e1afca
paulolirio <paulolirio@n46v.schedulesoftware>
2018-08-14 19:23:40 -0300

    Melhoria de código

--- introducao-javascript/js/calcula-imc.js
+++ introducao-javascript/js/calcula-imc.js
@@ -1,34 +1,38 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var pacientes = document.querySelectorAll(".paciente");
-for(var i = 0; i < pacientes.length; i++) {
-  var paciente = pacientes[i];
-   var tdPeso = paciente.querySelector(".info-peso");
-   var tdAltura = paciente.querySelector(".info-altura");
-   var tdImc = paciente.querySelector(".info-imc");
+validaDados();
 
-   var peso = tdPeso.textContent;
-   var altura = tdAltura.textContent;
+function validaDados() {
+  var pacientes = document.querySelectorAll(".paciente");
+  for(var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+     var tdPeso = paciente.querySelector(".info-peso");
+     var tdAltura = paciente.querySelector(".info-altura");
+     var tdImc = paciente.querySelector(".info-imc");
 
-   var alturaEhValida = true;
-   var pesoEhValido = true;
+     var peso = tdPeso.textContent;
+     var altura = tdAltura.textContent;
 
-   if (peso <= 0 || peso >= 1000) {
-     tdImc.textContent = "Peso inválido";
-     pesoEhValido = false;
-     paciente.classList.add("paciente-invalido");
-   }
+     var alturaEhValida = true;
+     var pesoEhValido = true;
 
-   if (altura <= 0 || altura >= 3) {
-     tdImc.textContent = "Altura inválida";
-     alturaEhValida = false;
-     paciente.classList.add("paciente-invalido");
-   }
+     if (peso <= 0 || peso >= 1000) {
+       tdImc.textContent = "Peso inválido";
+       pesoEhValido = false;
+       paciente.classList.add("paciente-invalido");
+     }
 
-   if (pesoEhValido && alturaEhValida) {
-     var imc = calculaImc(peso, altura);
-    tdImc.textContent = imc;
+     if (altura <= 0 || altura >= 3) {
+       tdImc.textContent = "Altura inválida";
+       alturaEhValida = false;
+       paciente.classList.add("paciente-invalido");
+     }
+
+     if (pesoEhValido && alturaEhValida) {
+       var imc = calculaImc(peso, altura);
+      tdImc.textContent = imc;
+    }
   }
 }
 
